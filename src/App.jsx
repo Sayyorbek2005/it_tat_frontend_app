ce9cdc0a15908fd90b6595d5d2edabcc4a17c094
Shakhzod1996 <shaxzod1996shax@gmail.com>
2025-08-14 22:36:27 +0500

    seo second

--- src/App.jsx
+++ src/App.jsx
@@ -18,30 +18,29 @@ import NotFound from './pages/notFound/NotFound'
 import OnlineCourses from './pages/onlineCourses/OnlineCourses'
 import ProgrammingCourse from './pages/programmingCourses/ProgrammingCourse'
 import Teachers from './pages/Teachers/Teachers'
-// import RoyxatdanOtish from './'
+import RoyxatdanOtish from './'
 // Lazy yuklanadigan sahifalar
-// const Kurslar = lazy(() => import('./pages/coursesAndPrice/Kurslar'))
-// const ProgrammingCourse = lazy(() =>
-// 	import('./pages/programmingCourses/ProgrammingCourse')
-// )
-// const LichTeacher = lazy(() => import('./pages/lichTeachers/LichTeacher'))
-// const Teachers = lazy(() => import('./pages/Teachers/Teachers'))
-// const BoshSahifa = lazy(() => import('./pages/boshSahifa/BoshSahifa'))
-// const OnlineCourses = lazy(() => import('./pages/onlineCourses/OnlineCourses'))
-// const NotFound = lazy(() => import('./pages/notFound/NotFound'))
+const Kurslar = lazy(() => import('./pages/coursesAndPrice/Kurslar'))
+const ProgrammingCourse = lazy(() =>
+	import('./pages/programmingCourses/ProgrammingCourse')
+)
+const LichTeacher = lazy(() => import('./pages/lichTeachers/LichTeacher'))
+const Teachers = lazy(() => import('./pages/Teachers/Teachers'))
+const BoshSahifa = lazy(() => import('./pages/boshSahifa/BoshSahifa'))
+const OnlineCourses = lazy(() => import('./pages/onlineCourses/OnlineCourses'))
+const NotFound = lazy(() => import('./pages/notFound/NotFound'))
 
 const App = () => {
-	// const location = useLocation()
+	const location = useLocation()
 
 	// Sahifa har gal o‘zgarganda yuqoriga chiqish
-	// useEffect(() => {
-	// 	window.scrollTo(0, 0)
-	// }, [location.pathname])
+	useEffect(() => {
+		window.scrollTo(0, 0)
+	}, [location.pathname])
 
 	return (
 		<>
-	<h1>Hello Man</h1>
-			{/* <ScrollToTop />
+			<ScrollToTop />
 			<Header />
 
 			<Suspense fallback={<Loading />}>
@@ -58,8 +57,7 @@ const App = () => {
 				</Routes>
 			</Suspense>
 
-			<Footer /> */}
-
+			<Footer />
 			{/* Toast xabarlari */}
 			<ToastContainer
 				position='top-right'
