bcf71232615592de973943b2d80c08039914e210
Shakhzod1996 <shaxzod1996shax@gmail.com>
2025-08-15 21:35:12 +0500

    new push

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
 // src/App.jsx
-import React, { lazy, Suspense, useEffect } from 'react';
-import { Routes, Route, useLocation } from 'react-router-dom';
+import { Suspense, useEffect } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
-import Loading from './Loading';
-import Header from './pages/boshSahifa/header/Header';
-import Footer from './pages/boshSahifa/footer/Footer';
 import ScrollToTop from './components/ScrollToTop';
+import Loading from './Loading';
+import Footer from './pages/boshSahifa/footer/Footer';
+import Header from './pages/boshSahifa/header/Header';
+
+import BoshSahifa from './pages/boshSahifa/BoshSahifa';
+import Registration from './pages/boshSahifa/registration/Registration';
+import Kurslar from './pages/coursesAndPrice/Kurslar';
+import LichTeacher from './pages/lichTeachers/LichTeacher';
+import NotFound from './pages/notFound/NotFound';
+import OnlineCourses from './pages/onlineCourses/OnlineCourses';
+import ProgrammingCourse from './pages/programmingCourses/ProgrammingCourse';
+import Teachers from './pages/Teachers/Teachers';
 
 import 'antd/dist/reset.css';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
-
-// Lazy-loaded pages (faqat bitta usul!)
-const BoshSahifa = lazy(() => import('./pages/boshSahifa/BoshSahifa'));
-const Registration = lazy(() => import('./pages/boshSahifa/registration/Registration'));
-const Kurslar = lazy(() => import('./pages/coursesAndPrice/Kurslar'));
-const LichTeacher = lazy(() => import('./pages/lichTeachers/LichTeacher'));
-const NotFound = lazy(() => import('./pages/notFound/NotFound'));
-const OnlineCourses = lazy(() => import('./pages/onlineCourses/OnlineCourses'));
-const ProgrammingCourse = lazy(() => import('./pages/programmingCourses/ProgrammingCourse'));
-const Teachers = lazy(() => import('./pages/Teachers/Teachers'));
 
 function App() {
   const location = useLocation();
